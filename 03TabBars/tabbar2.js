033a32b1e4d212de6aa378aa01ac09415b0745e9
gondr99 <gondr99@gmail.com>
2023-01-16 16:49:48 +0900

    transition

--- 03TabBars/tabbar2.js
+++ 03TabBars/tabbar2.js
@@ -1,10 +1,15 @@
 window.onload = () => {
     const tabs = document.querySelectorAll(".tab");
 
-    tabs.forEach(tab => {
+    const boxes = document.querySelectorAll(".box");
+
+    tabs.forEach( (tab, idx) => {
         tab.addEventListener("click", e => {
             tabs.forEach(t => t.classList.remove("on"));
             tab.classList.add("on");
+
+            boxes.forEach( b => b.classList.remove("on"));
+            boxes[idx].classList.add("on");
         });
     });
 };
