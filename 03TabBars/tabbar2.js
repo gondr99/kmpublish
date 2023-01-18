4f624a960a5dfecfbf3561d409cce5c64df9baa6
gondr99 <gondr99@gmail.com>
2023-01-18 12:29:55 +0900

    add ajax

--- 03TabBars/tabbar2.js
+++ 03TabBars/tabbar2.js
@@ -1,15 +1,25 @@
 window.onload = () => {
     const tabs = document.querySelectorAll(".tab");
-
+    const content = document.querySelector(".content");
     const boxes = document.querySelectorAll(".box");
+
+    let currentIdx = 0;
 
     tabs.forEach( (tab, idx) => {
         tab.addEventListener("click", e => {
             tabs.forEach(t => t.classList.remove("on"));
             tab.classList.add("on");
 
-            boxes.forEach( b => b.classList.remove("on"));
+            boxes[currentIdx].classList.add("off");
+            let before = currentIdx; //저장해두고
+            setTimeout(()=>{
+                content.removeChild(boxes[before]);
+                boxes[before].classList.remove("on", "off");
+                content.appendChild(boxes[before]);
+            }, 500);//0.5초 애니메이션이니까
+
             boxes[idx].classList.add("on");
+            currentIdx = idx;
         });
     });
 };
---
+++ 05ajaxTest/app.js
@@ -0,0 +1,5 @@
+// ajax 샘플 사이트 https://reqres.in/
+
+window.onload = ()=>{
+    
+};
